91b811f0be34b62b7bef1acd9760b58794f7f856
MichelHBFilho <mbf27@outlook.com>
2021-10-22 23:17:16 -0300

    Tudo funcionando, falta definir o fim de jogo

---
+++ forca.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "forca.hpp"
+
+using namespace std;
+
+void imprimeCabecalho() {
+    cout << "        .-'''-.       " << endl;
+    cout << "        /       \\    " << endl;
+    cout << "        \\       /    " << endl;
+    cout << ".-'''-.-`.-.-.<  _    " << endl;
+    cout << "/      _,-\\ ()()_/:) ";
+    cout << "  Jogo da forca"        << endl;
+    cout << "\\     / ,  `     `|   ";
+    cout << "  BY MICHEL FILHO"      << endl;
+    cout << "'-..-| \\-.,___,  /    " << endl;
+    cout << "    \\ `-.__/  /       " << endl;
+    cout << "      `-.__.-'`       " << endl;
+}
+
+int main() {
+    imprimeCabecalho();
+}
---
+++ forca.hpp
@@ -0,0 +1,6 @@
+#ifndef _FORCA_HPP_
+#define _FORCA_HPP_
+
+void imprimeCabecalho();
+
+#endif
